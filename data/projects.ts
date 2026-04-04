d8471aeb62f38beb6bb77511559f8ea1027c0f76
Leigh Michael Forrest <leighmforrest@gmail.com>
2026-04-04 15:00:52 -0400

    two projects showing

--- data/projects.ts
+++ data/projects.ts
@@ -31,26 +31,6 @@ const projects: Project[] = [
       { name: "FaPython", set: "fa6" },
     ],
   },
-  {
-    name: "seed 3",
-    imageUrl: "https://placehold.co/600x600/png",
-    githubUrl: "https://github.com/leighmforrest/seed",
-    icons: [
-      { name: "FaPython", set: "fa6" },
-      { name: "FaReact", set: "fa6" },
-      { name: "BsTypescript", set: "bs" },
-    ],
-  },
-  {
-    name: "seed 4",
-    imageUrl: "https://placehold.co/600x600/png",
-    githubUrl: "https://github.com/leighmforrest/seed",
-    icons: [
-      { name: "FaPython", set: "fa6" },
-      { name: "FaReact", set: "fa6" },
-      { name: "BsTypescript", set: "bs" },
-    ],
-  },
 ];
 
 export default projects;
